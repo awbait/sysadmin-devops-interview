ee7da38574c1127ef4f5b71d84e2a2ccb6785b0e
awbait <awbait.com@gmail.com>
2025-09-09 15:00:09 +0300

    fix: errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,24 @@
-import '@/app/global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
+import '@/app/global.css'
+import SearchDialog from '@/components/search'
+import { RootProvider } from 'fumadocs-ui/provider'
+import { Inter } from 'next/font/google'
 
 const inter = Inter({
-  subsets: ['latin'],
-});
+	subsets: ['latin'],
+})
 
 export default function Layout({ children }: LayoutProps<'/'>) {
-  return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en' className={inter.className} suppressHydrationWarning>
+			<body className='flex flex-col min-h-screen'>
+				<RootProvider
+					search={{
+						SearchDialog,
+					}}
+				>
+					{children}
+				</RootProvider>
+			</body>
+		</html>
+	)
 }
